5dc5e40ac1e3edc04980c24078d48b064763adcf
mauricio-testa <iciotesta@hotmail.com>
2023-04-26 22:52:39 -0300

    add footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,15 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
-  imports: [
+  imports: [ // como se fosse o components {} do vue
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+	MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent] // #2 O app.module.ts diz que o componente inicial é o AppComponent. Como se fosse o App.vue
