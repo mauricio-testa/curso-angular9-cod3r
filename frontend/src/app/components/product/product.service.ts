6e88865d5fc4df7f0acee94585cbc663e45d34a9
mauricio-testa <iciotesta@hotmail.com>
2023-05-05 21:33:03 -0300

    display backend errors

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, catchError, map } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root' // Pode ser injetado em toda a aplicação
@@ -20,30 +20,51 @@ export class ProductService {
 	) { }
 
 	index(): Observable<Array<Product>> {
-		return this.http.get<Array<Product>>(this.baseUrl);
+		return this.http.get<Array<Product>>(this.baseUrl).pipe(
+			map((obj) => obj),
+			catchError((e) => this.errorHandler(e))
+		)
 	}
 
 	create(product: Product): Observable<Product> {
-		return this.http.post<Product>(this.baseUrl, product)
+		return this.http.post<Product>(this.baseUrl, product).pipe(
+			map((obj) => obj),
+			catchError((e) => this.errorHandler(e))
+		)
 	}
 
 	get(id: number): Observable<Product> {
-		return this.http.get<Product>(`${this.baseUrl}/${id}`)
+		return this.http.get<Product>(`${this.baseUrl}/${id}`).pipe(
+			map((obj) => obj),
+			catchError((e) => this.errorHandler(e))
+		)
 	}
 
 	update(product: Product): Observable<Product> {
-		return this.http.put<Product>(`${this.baseUrl}/${product.id}`, product)
+		return this.http.put<Product>(`${this.baseUrl}/${product.id}`, product).pipe(
+			map((obj) => obj),
+			catchError((e) => this.errorHandler(e))
+		)
 	}
 
 	delete(id: number): Observable<Product> {
-		return this.http.delete<Product>(`${this.baseUrl}/${id}`)
+		return this.http.delete<Product>(`${this.baseUrl}/${id}`).pipe(
+			map((obj) => obj),
+			catchError((e) => this.errorHandler(e))
+		)
 	}
 
-	showMessage(msg: string): void {
+	showMessage(msg: string, isError: boolean = false): void {
 		this.snackBar.open(msg, 'X', {
-			duration: 3000,
+			duration: 30000,
 			horizontalPosition: "right",
-			verticalPosition: "top"
+			verticalPosition: "top",
+			panelClass: isError ? 'toast-error' : 'toast-success'
 		})
+	}
+
+	errorHandler(e: any): Observable<any> {
+		this.showMessage(e.message, true)
+		return EMPTY
 	}
 }
